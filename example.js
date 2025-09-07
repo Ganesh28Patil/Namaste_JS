2a27865861d391a4df6256584cfc776105d2f87d
Ganesh Patil <gp28info@gmail.com>
2025-09-07 09:05:42 +0530

    first Class Citizens

--- example.js
+++ example.js
@@ -10,17 +10,39 @@
 //     }, j*1000);   
 // }
 
-function x(){
-    for(var i = 1; i <= 5; i++){
-        function closure(x){
-            setTimeout(function() {
-                console.log(x);
-            }, x * 1000);
-        }
-        closure(i);
-    }
+// function x(){
+//     for(var i = 1; i <= 5; i++){
+//         function closure(x){
+//             setTimeout(function() {
+//                 console.log(x);
+//             }, x * 1000);
+//         }
+//         closure(i);
+//     }
 
-    console.log('Namaste Javascript | setTimeOut');
+//     console.log('Namaste Javascript | setTimeOut');
+// }
+
+// x();
+
+
+// =---=-=--=-=--=---=---=---=---=---=---=---=---=---=
+//  Difference between Fucntion Statement & Function Expression is Hoisting 
+
+// a(); // invoking function a()  //  a invoked
+console.log(a());    //  a invoked
+console.log(b());    // undefined
+// b(); // invoking function b() //  TypeError: b is not a function
+
+ // Function Statement
+function a(){
+    console.log('a invoked');
 }
+// a(); // invoking function a()
 
-x();
+// Function Expression 
+var b = function(){
+    console.log('b invoked');
+}
+// b(); // invoking function b()
+
